4a6accf7be094ee027e37a1b95ff122c442c359c
Albertomartin21 <alberto_martin_21@hotmail.com>
2022-02-01 14:27:51 +0100

    feat: Add Spring proyect

---
+++ StreetSneakers/src/main/java/com/example/demo/StreetSneakersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetSneakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetSneakersApplication.class, args);
+	}
+
+}
